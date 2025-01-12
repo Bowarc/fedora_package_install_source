0043c893dd22a3d33a7aa2c02025de40f28e4542
Bowarc <yuyeaaaahhhh@laposte.net>
2025-01-12 22:09:07 +0100

    Small cleaning

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def main():
         sys.exit(1)
 
     target_package = str(sys.argv[1])
-
     found: bool = False
 
     processes = [
@@ -26,33 +25,33 @@ def main():
 
     for packet_mgr, process in processes:
         output = process.communicate()[0]
+
         if output == b"":
-            print(f"{packet_mgr} did not output anything")
+            print(f"{packet_mgr} did not output anything", file=sys.stderr)
             continue
 
         if target_package.encode().lower() not in output.lower():
-            # print(f"{packet_mgr} did not output anything")
             continue
 
         print(f"Package {target_package} was installed using {packet_mgr}")
         found = True
+        # In case the user has a weird install and multiple packet mgr find the same package, i won't add any break here
 
     if found:
         return
 
     for path in os.environ.get("PATH").split(":"):
         if not os.path.exists(path):
-            # print(f"[WARN] '{path}' does not exist")
+            # print(f"[WARN] '{path}' does not exist", file=sys.stderr)
             continue
         for file in os.listdir(path):
             if target_package.lower() in file.lower():
                 print(f"Found {file} in {path}")
                 found = True
+                # Same thing here
 
     if not found:
-
         print(f"Could not find the packet mgr {target_package} was installed with")
-
 
 if __name__ == "__main__":
     main()
